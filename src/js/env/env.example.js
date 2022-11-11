c85fcbc87ba042db7f4d5bd18195197caa6d51f4
Carlos Diaz <carlosdiaz.devx@gmail.com>
2022-11-11 16:58:49 -0600

    Development enviroment setup

---
+++ src/js/env/env.example.js
@@ -0,0 +1,6 @@
+
+// 1. Renombra o crea un nuevo archivo con el nombre [env.js]
+// 2. Crea una cuenta en The MovieDB y genera tu API key: https://www.themoviedb.org/signup
+// 3. Ingresa tu API key aquí.
+
+const API_KEY = "";
